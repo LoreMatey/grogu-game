64895717cb980d234f7f6a2f99a55b778bb181df
LoreMatey <lorena.matey.l@gmail.com>
2022-01-21 04:01:47 +0100

    Added LinkNav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import './Navbar.css';
 // import { FaBars } from 'react-icons/fa';
-// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const navItem1 = 'Reglas';
@@ -11,11 +10,25 @@ const Navbar = () => {
   return (
     <>
       <div className='navbar'>
-      <img className='navbar-logo' src='https://www.creativefabrica.com/wp-content/uploads/2021/08/20/bayoda-grogu-svg-Graphics-16076611-1-1-580x435.jpg' />
+        <NavLink to='/'>
+          <img className='navbar-logo' src='https://www.creativefabrica.com/wp-content/uploads/2021/08/20/bayoda-grogu-svg-Graphics-16076611-1-1-580x435.jpg' />
+        </NavLink>
         <ul className='navbar-list'>
-          <li className='navbar-item'>{navItem1}</li>
-          <li className='navbar-item'>{navItem2}</li>
-          <li className='navbar-item'>{navItem3}</li>
+          <li className='navbar-item'>
+            <NavLink to='/rules'>
+              {navItem1}
+            </NavLink>
+          </li>
+          <li className='navbar-item'>
+            <NavLink to='/pieces'>
+              {navItem2}
+            </NavLink>
+          </li>
+          <li className='navbar-item'>
+            <NavLink to='/game'>
+              {navItem3}
+            </NavLink>
+          </li>
         </ul>
       </div>
     </>
