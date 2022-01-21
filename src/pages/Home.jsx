02a883ed9d90b73d55c1ae06f266dcb3b480b0da
LoreMatey <lorena.matey.l@gmail.com>
2022-01-21 03:36:19 +0100

    Added HomeData component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Home.css'
+import HomeData from './HomeData';
 
 const Home = () => {
+
   return (
     <div className='homepage-wrapper'>
+      <HomeData />
     </div>
     
   )
---
+++ src/pages/HomeData.jsx
@@ -0,0 +1,22 @@
+import './HomeData.css';
+const HomeData = () => {
+  return (
+    <div className="homepage-info--wrapper">
+      <h1>¡Cuidado con Goru!</h1>
+        <p>
+          El cazarrecompensas mandaloriano Din Djarin, conocido como «el Mandaloriano» o simplemente «mando», está a cargo de Grogu mientras viaja por la galaxia realizando todo tipo de encargos. 
+        </p>
+        <p>
+          En esta ocasión el encargo es muy especial: debe entregar huevos de rana (no fertilizados), galletas azules (¡qué ricas!) y unas ranas. Este cargamento es de vital importancia para restablecer contacto con Ahsoka Tano. 
+        </p>
+        <p>
+          El objetivo es entregar la mercancía a un contacto en el planeta Nevarro; él nos dará la información del paradero de Ahsoka, información que usaremos para volver a intentar que sea la maestra de Grogu como Jedi. El problema es que nuestro contacto ha sido muy claro: si no llega la mercancía, no hay trato. 
+        </p>
+        <p>
+          Tu misión es tratar que Grogu no encuentre la mercancía. Mando ha recibido muy buenas recomendaciones sobre tus habilidades custodiando mercancía así que ha decidido contratarte para que todo salga bien. Ayuda a Mando a conseguir su objetivo y tendrás una buena recompensa.
+        </p>
+    </div>
+  )
+}
+
+export default HomeData;
